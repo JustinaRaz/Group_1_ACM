d77e146d1a2845ea90d6d5d73f976298257a34db
Rohambarack <szbalage00@gmail.com>
2025-02-18 10:12:04 +0100

    adding the heigth example too

--- Assignment_2/test.R
+++ Assignment_2/test.R
@@ -1,5 +1,6 @@
 library(cmdstanr)
 file <- file.path("models/biased_random.stan")
+fileh <- file.path("models/cont_height.stan")
 
 
 #make data
@@ -41,4 +42,37 @@ samples <- mod$sample(
   adapt_delta = 0.99, # how high a learning rate to adjust hyperparameters during warmup
 )
 
+################ 
 
+h_data <- list(
+  n = 40,
+  h = rnorm(40,180,5)
+)
+
+# Compile the model
+modh <- cmdstan_model(fileh, 
+                     # this specifies we can parallelize the gradient estimations on multiple cores
+                     cpp_options = list(stan_threads = TRUE), 
+                     # this is a trick to make it faster
+                     stanc_options = list("O1")) 
+
+# The following command calls Stan with specific options.
+samples_h <- modh$sample(
+  data = h_data, # the data :-)
+  seed = 123,  # a seed, so I always get the same results
+  chains = 4,  # how many chains should I fit (to check whether they give the same results)
+  parallel_chains = 4, # how many of the chains can be run in parallel?
+  threads_per_chain = 1, # distribute gradient estimations within chain across multiple cores
+  iter_warmup = 1000,  # warmup iterations through which hyperparameters (steps and step length) are adjusted
+  iter_sampling = 2000, # total number of iterations
+  refresh = 200,  # how often to show that iterations have been run
+  #output_dir = "simmodels", # saves the samples as csv so it can be later loaded
+  max_treedepth = 20, # how many steps in the future to check to avoid u-turns
+  adapt_delta = 0.99, # how high a learning rate to adjust hyperparameters during warmup
+)
+
+samples_h$summary()
+
+#curve function is very good for quickly checking distribuitons,
+#and how their prameters affect their density 
+curve(dgamma(x,2,0.4), from = 0, to = 10)
