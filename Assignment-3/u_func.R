4fba97f7c6e8579039571979fe5eb782f72c6617
Rohambarack <szbalage00@gmail.com>
2025-03-18 09:11:59 +0100

    assign "group" rating function added

--- Assignment-3/u_func.R
+++ Assignment-3/u_func.R
@@ -4,3 +4,14 @@ library(tidyverse)
 sim_data <- function(){
   
 }
+
+assign_GR <- function(FaceRating){
+  #get relative feedback
+  F_B <- sample(c(-3,-2,0,2,3),1)
+  #assign as group rating, round to 0 and 8
+  G_R <- FaceRating + F_B
+  G_R <- ifelse(G_R < 0,0,
+         ifelse(G_R > 8, 8, G_R))
+  
+  return(G_R)
+}
